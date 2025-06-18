134d45cae183eff5803b1460ac2d6d303b6f2687
Patrick Orave <42103411+linkion@users.noreply.github.com>
2025-06-18 14:15:56 -0500

    Add Dart Sass to the development environment packages

--- devenv.nix
+++ devenv.nix
@@ -5,7 +5,7 @@
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git pkgs.hugo pkgs.husky pkgs.obsidian-export ];
+  packages = [ pkgs.git pkgs.hugo pkgs.husky pkgs.obsidian-export pkgs.dart-sass ];
 
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
